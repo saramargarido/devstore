48295062b2d3bc0b5d33feca4132b64c7d00f47f
Sara Margarido <saramargarido@MacBook-Pro-de-Sara.local>
2025-01-15 19:58:26 +0100

    add header

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,8 +17,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt" className={`${inter.variable} antialiased`}>
-      <body>{children}</body>
+    <html lang="pt" className={inter.variable}>
+      <body className="bg-zinc-950 text-zinc-50 antialiased">{children}</body>
     </html>
   );
 }
