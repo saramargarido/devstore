136847e89eae98a12dde5eb9ba4779852a218865
saramargarido <saramargarido@gmail.com>
2025-01-20 22:47:16 +0100

    loading in home

--- src/app/(store)/(home)/loading.tsx
+++ src/app/(store)/(home)/loading.tsx
@@ -1,3 +1,11 @@
+import { Skeleton } from '@/components/skeleton'
+
 export default function HomeLoading() {
-  return <div>carregando</div>
+  return (
+    <div className="grid h-full grid-cols-6 gap-6">
+      <Skeleton className="col-span-6 row-span-6 h-[856px]"></Skeleton>
+      <Skeleton className="col-span-6 row-span-3"></Skeleton>
+      <Skeleton className="col-span-6 row-span-3"></Skeleton>
+    </div>
+  )
 }
--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -1,6 +1,7 @@
 import data from '../data.json'
 
 export async function GET() {
+  await new Promise((resolve) => setTimeout(resolve, 1000))
   const featuredProducts = data.products.filter((products) => products.featured)
 
   return Response.json(featuredProducts)
