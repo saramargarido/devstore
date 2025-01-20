1cf159878078668ce93c0901120d1a8ae30d24d1
saramargarido <saramargarido@gmail.com>
2025-01-20 23:34:51 +0100

    fetch in product data

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -47,7 +47,7 @@ export default async function Home() {
       {otherProducts.map((product) => (
         <Link
           key={product.id}
-          href={`/product/${highlightedProduct.slug}`}
+          href={`/product/${product.slug}`}
           className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
         >
           <Image
