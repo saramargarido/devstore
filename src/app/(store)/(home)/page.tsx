64377b58e159cb965a655445ea25f09bab68e12d
saramargarido <saramargarido@gmail.com>
2025-01-20 22:20:11 +0100

    add key in map component

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -46,6 +46,7 @@ export default async function Home() {
       </Link>
       {otherProducts.map((product) => (
         <Link
+          key={product.id}
           href={`/product/${highlightedProduct.slug}`}
           className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
         >
